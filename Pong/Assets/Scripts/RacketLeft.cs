fbac1eb81b46875d196134a34c0a34f7dab05810
layse araújo <layse.araujo.lr@gmail.com>
2021-05-04 12:12:32 -0300

    pong

---
+++ Pong/Assets/Scripts/RacketLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketLeft : MonoBehaviour {
+ 
+    public float speed;
+    public string axis;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (0, v) * speed;
+    }
+}
